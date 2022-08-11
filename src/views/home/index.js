174071750d6883cd0217ce681fc964e576b16ccb
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-11 12:01:23 -0400

    :sparkles: [ADD] home page interface
    Now you can get a preview for your next platzi punk

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,26 +1,155 @@
+import {
+  Stack,
+  Flex,
+  Heading,
+  Text,
+  Button,
+  Image,
+  Badge,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { useWeb3React } from "@web3-react/core";
-import React, { useEffect } from "react";
-import { useCallback } from "react";
-import { useState } from "react";
 import usePlatziPunks from "../../hooks/usePlatziPunks";
+import { useCallback, useEffect, useState } from "react";
 
-export default function Home() {
-  const [maxSupply, setMaxSupply] = useState();
-  const { active } = useWeb3React();
+const Home = () => {
+  const [imageSrc, setImageSrc] = useState("");
+  const [availablePlatziPunks, setAvailablePlatziPunks] = useState();
+
+  const { active, account } = useWeb3React();
   const platziPunks = usePlatziPunks();
 
-  const getMaxSupply = useCallback(async () => {
+  const getPlatziPunksData = useCallback(async () => {
     if (platziPunks) {
-      const result = await platziPunks.methods.maxSupply().call();
-      setMaxSupply(result);
+      const totalSupply = await platziPunks.methods.totalSupply().call();
+      const maxSupply = await platziPunks.methods.maxSupply().call();
+      const dnaPreview = await platziPunks.methods
+        .deterministicPseudoRandomDNA(totalSupply, account)
+        .call();
+      const image = await await platziPunks.methods
+        .imageByDNA(dnaPreview)
+        .call();
+      setImageSrc(image);
+      setAvailablePlatziPunks(maxSupply - totalSupply);
     }
-  }, [platziPunks]);
+  }, [platziPunks, account]);
 
   useEffect(() => {
-    getMaxSupply();
-  }, [getMaxSupply]);
+    getPlatziPunksData();
+  }, [getPlatziPunksData]);
 
-  if (!active) return "Conecta tu wallet";
+  return (
+    <Stack
+      align={"center"}
+      spacing={{ base: 8, md: 10 }}
+      py={{ base: 20, md: 28 }}
+      direction={{ base: "column-reverse", md: "row" }}
+    >
+      <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+        <Heading
+          lineHeight={1.1}
+          fontWeight={600}
+          fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
+        >
+          <Text
+            as={"span"}
+            position={"relative"}
+            _after={{
+              content: "''",
+              width: "full",
+              height: "30%",
+              position: "absolute",
+              bottom: 1,
+              left: 0,
+              bg: "green.400",
+              zIndex: -1,
+            }}
+          >
+            Un Platzi Punk
+          </Text>
+          <br />
+          <Text as={"span"} color={"green.400"}>
+            nunca para de aprender
+          </Text>
+        </Heading>
+        <Text color={"gray.500"}>
+          Platzi Punks es una colección de Avatares randomizados cuya metadata
+          es almacenada on-chain. Poseen características únicas y sólo hay 10000
+          en existencia.
+        </Text>
+        <Text color={"green.500"}>
+          Cada Platzi Punk se genera de forma secuencial basado en tu address,
+          usa la previsualización para averiguar cuál sería tu Platzi Punk si
+          hace mint en este momento.
+        </Text>
+        <Stack
+          spacing={{ base: 4, sm: 6 }}
+          direction={{ base: "column", sm: "row" }}
+        >
+          <Button
+            rounded={"full"}
+            size={"lg"}
+            fontWeight={"normal"}
+            px={6}
+            colorScheme={"green"}
+            bg={"green.400"}
+            _hover={{ bg: "green.500" }}
+            disabled={!platziPunks || availablePlatziPunks === 0}
+          >
+            Obtén tu punk
+          </Button>
+          <Link to="/punks">
+            <Button rounded={"full"} size={"lg"} fontWeight={"normal"} px={6}>
+              Galería
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
+      <Flex
+        flex={1}
+        direction="column"
+        justify={"center"}
+        align={"center"}
+        position={"relative"}
+        w={"full"}
+      >
+        <Image src={active ? imageSrc : "https://avataaars.io/"} />
+        {active ? (
+          <>
+            <Flex mt={2}>
+              <Badge>
+                Next ID:
+                <Badge ml={1} colorScheme="green">
+                  1
+                </Badge>
+              </Badge>
+              <Badge ml={2}>
+                Address:
+                <Badge ml={1} colorScheme="green">
+                  0x0000...0000
+                </Badge>
+              </Badge>
+            </Flex>
+            <Badge mt={2}>
+              {availablePlatziPunks > 0
+                ? `Quedan ${availablePlatziPunks} Platzi Punks por mintear!`
+                : `Se agotaron los Platzi Punks :(`}
+            </Badge>
+            <Button
+              onClick={getPlatziPunksData}
+              mt={4}
+              size="xs"
+              colorScheme="green"
+            >
+              Actualizar
+            </Button>
+          </>
+        ) : (
+          <Badge mt={2}>Wallet desconectado</Badge>
+        )}
+      </Flex>
+    </Stack>
+  );
+};
 
-  return <p>Max Supply: {maxSupply}</p>;
-}
+export default Home;
